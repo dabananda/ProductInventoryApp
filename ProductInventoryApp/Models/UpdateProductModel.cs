38980fd3b0aa305c9e24d40209920512cef99c9b
dabananda <imdmitra@gmail.com>
2024-12-26 15:57:55 +0600

    Updated model names

---
+++ ProductInventoryApp/Models/UpdateProductModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ProductInventoryApp.Models
+{
+    public class UpdateProductModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public string QuantityUnit { get; set; }
+    }
+}
